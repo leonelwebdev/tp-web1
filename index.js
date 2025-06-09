54336e72fc8df4653c6df03f8a596687ad05b170
Leonel Rodriguez <leonelrodrigguez@gmail.com>
2025-06-09 18:34:37 -0300

    agregamos formulario

--- index.js
+++ index.js
@@ -7,3 +7,26 @@
     document.body.classList.toggle('dark-mode');
     toggleBtn.textContent = isDarkMode ? '🌙' : '🌞';
   });
+
+  // Validación de formulario
+  const form = document.getElementById('newsletter-form');
+  const emailInput = document.getElementById('email');
+  const message = document.getElementById('form-message');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    const email = emailInput.value;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (regex.test(email)) {
+      message.textContent = "¡Gracias por suscribirte!";
+      message.style.color = "green";
+      message.style.fontSize = "1.5rem";
+      emailInput.value = "";
+    } else {
+      message.textContent = "Ingresá un correo válido.";
+      message.style.color = "red";
+      message.style.fontSize = "1.5rem";
+    }
+  });
